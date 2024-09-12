273fde640538b295e1b91b7551f2ef4c1ae38a6b
Abhi Varde <91776428+AbhiVarde@users.noreply.github.com>
2024-09-12 10:59:23 +0530

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,3 @@
 export const GITHUB_URL = "https://github.com/AbhiVarde/syncui";
 export const TWITTER_URL = "https://twitter.com/your-account";
+export const INSTAGRAM_URL = "https://instagram.com/your-account";
