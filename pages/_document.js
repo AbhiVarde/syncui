db14d4f90fb141268833a26470a13dcaa2ad1b28
Abhi Varde <91776428+AbhiVarde@users.noreply.github.com>
2024-10-03 21:09:31 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,35 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { DefaultSeo } from "next-seo";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <DefaultSeo
-          title="Sync UI"
-          description="A sleek UI library for Design Engineers, offering beautifully designed components built with MUI and Framer Motion."
-          canonical="https://www.syncui.design/"
-          openGraph={{
-            url: "https://www.syncui.design/",
-            title: "Sync UI",
-            description: "A sleek UI library for Design Engineers, offering beautifully designed components built with MUI and Framer Motion.",
-            images: [
-              {
-                url: "https://www.syncui.design/default-og-image.png",
-                width: 1200,
-                height: 630,
-                alt: "Sync UI - Sleek UI Library for Design Engineers",
-              },
-            ],
-            siteName: "Sync UI",
-          }}
-          twitter={{
-            cardType: "summary_large_image",
-            site: "@syncuidesign",
-            handle: "@syncuidesign",
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
