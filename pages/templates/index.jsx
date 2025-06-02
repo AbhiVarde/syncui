df77b46fdfa8bda38b98c66e0e2c60f04c3e8677
Abhi Varde <abhivarde99@gmail.com>
2025-06-02 22:48:24 +0530

    feat: remove guestbook and add sponsor buttons to landing and docs pages

--- pages/templates/index.jsx
+++ pages/templates/index.jsx
@@ -257,6 +257,7 @@ const Templates = () => {
               </MotionTypography>
 
               {/* Offer Chip */}
+            
               <MotionBox
                 whileHover={{ scale: 1.03 }}
                 transition={{ type: "spring", stiffness: 300, damping: 20 }}
