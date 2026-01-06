c98bf2abaab8c92354952f8c341ef09debd2ae96
AbhiVarde <abhivarde99@gmail.com>
2026-01-06 17:10:23 +0530

    feat: add getStaticProps to home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { getAllDocsSlugs } from "@/lib/docs";
 import HeroSection from "@/components/home/HeroSection";
 import StargazersSection from "@/components/home/StargazersSection";
 import FeaturesSection from "@/components/home/FeaturesSection";
 
-const Home = () => {
+const Home = ({ docsTree }) => {
   return (
     <>
       <HeroSection />
@@ -14,3 +15,13 @@ const Home = () => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const docsTree = await getAllDocsSlugs();
+
+  return {
+    props: {
+      docsTree,
+    },
+  };
+}
