8d08753bacdafa028b69f74cf085bc44344fc136
AbhiVarde <abhivarde99@gmail.com>
2026-03-19 12:50:45 +0530

    chore: disable AdSense scripts temporarily

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
+        {/* <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4873815556142919"
           crossOrigin="anonymous"
-        ></script>
+        ></script> */}
       </Head>
       <body>
         <Main />
