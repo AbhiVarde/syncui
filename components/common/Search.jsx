8fa9ae9677f067318e91cef78770460034cca15d
AbhiVarde <abhivarde99@gmail.com>
2026-06-14 09:13:44 +0530

    style: refine search dialog and input styling

--- components/common/Search.jsx
+++ components/common/Search.jsx
@@ -250,13 +250,15 @@ const Search = ({ docsTree = [] }) => {
                 width: "100%",
                 height: "40px",
                 border: `1px solid ${isDark ? "#1F1F1F" : "#EEEEEE"}`,
-                borderRadius: "14px !important",
+                borderRadius: "14px",
                 background: isDark ? "#1A1A1A" : "#FAFAFA",
                 color: isDark ? "#FFFFFF" : "#111111",
                 padding: "0 40px",
                 outline: "none",
                 fontSize: "14px",
                 boxSizing: "border-box",
+                appearance: "none",
+                WebkitAppearance: "none",
               }}
             />
           </Box>
